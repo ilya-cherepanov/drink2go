26253682d9ce07bf5cdb38aee7b7dc0f41b90fd9
Ilya Cherepanov <cherepanov.ilya@gmail.com>
2026-05-01 01:35:22 +0700

    add header js

---
+++ source/scripts/constants.js
@@ -0,0 +1,4 @@
+export const NavButtonLabel = Object.freeze({
+  Opened: 'Закрыть меню навигации',
+  Closed: 'Открыть меню навигации',
+});
--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,4 @@
-/* в этот файл добавляет скрипты*/
+import { MainNavigation } from './components/navigation.js';
+
+const navigation = new MainNavigation();
+navigation.handleEvents();
