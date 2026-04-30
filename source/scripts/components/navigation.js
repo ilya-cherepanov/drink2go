5eb4b3f718f9bf808116466c94341c31ce98363d
Ilya Cherepanov <cherepanov.ilya@gmail.com>
2026-05-01 01:37:15 +0700

    Add header (#1)
    * add header markup
    
    * add header styles
    
    * add header js

---
+++ source/scripts/components/navigation.js
@@ -0,0 +1,28 @@
+import { NavButtonLabel } from '../constants.js';
+
+export class MainNavigation {
+  constructor() {
+    this._headerElement = document.querySelector('.header');
+    this._buttonElement = this._headerElement.querySelector('.header__action--menu');
+    this._buttonCaptionElement = this._buttonElement.querySelector('.header__action-caption');
+    this._navigationElement = this._headerElement.querySelector('.header__navigation');
+    this._opened = this._headerElement.classList.contains('header--navigation-open');
+  }
+
+  toggleNavigation() {
+    this._opened = !this._opened;
+    this._headerElement.classList.toggle('header--navigation-open', this._opened);
+    this._buttonCaptionElement.textContent = this._opened
+      ? NavButtonLabel.Opened
+      : NavButtonLabel.Closed;
+  }
+
+  handleEvents() {
+    this._buttonElement.addEventListener('click', this.#handleClick);
+  }
+
+  #handleClick = (evt) => {
+    evt.preventDefault();
+    this.toggleNavigation();
+  };
+}
