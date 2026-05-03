0bc4d51923fa93c14862f6531f1d028bde8b5100
Ilya Cherepanov <cherepanov.ilya@gmail.com>
2026-05-03 10:25:40 +0700

    add catalog styles

--- source/scripts/constants.js
+++ source/scripts/constants.js
@@ -2,3 +2,8 @@ export const NavButtonLabel = Object.freeze({
   Opened: 'Закрыть меню навигации',
   Closed: 'Открыть меню навигации',
 });
+
+export const Price = Object.freeze({
+  Min: 0,
+  Max: 967,
+});
--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,4 +1,16 @@
 import { MainNavigation } from './components/navigation.js';
+import { Price } from './constants.js';
 
 const navigation = new MainNavigation();
 navigation.handleEvents();
+
+const dualRange = document.querySelector('.dual-range');
+noUiSlider.create(dualRange, {
+  start: [0, 900],
+  connect: true,
+  // cssPrefix: 'noui-',
+  range: {
+    min: Price.Min,
+    max: Price.Max,
+  }
+});
