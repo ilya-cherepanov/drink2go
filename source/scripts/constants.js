2937634d3394ab73117649edb6e2b751aa2b81b7
Ilya Cherepanov <cherepanov.ilya@gmail.com>
2026-05-03 20:02:58 +0700

    Add Catalog (#4)
    * add products markup
    
    * add product catalog section styles
    
    * add filters markup
    
    * add catalog styles
    
    * fix linter errors
    
    * add range js

--- source/scripts/constants.js
+++ source/scripts/constants.js
@@ -2,3 +2,9 @@ export const NavButtonLabel = Object.freeze({
   Opened: 'Закрыть меню навигации',
   Closed: 'Открыть меню навигации',
 });
+
+export const Price = Object.freeze({
+  Min: 0,
+  Max: 967,
+});
+export const DEFAULT_MAX_PRICE = 900;
--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,4 +1,11 @@
 import { MainNavigation } from './components/navigation.js';
+import { Range } from './components/range.js';
+import { DEFAULT_MAX_PRICE, Price } from './constants.js';
 
 const navigation = new MainNavigation();
 navigation.handleEvents();
+
+const rangeElement = document.querySelector('.range');
+const range = new Range(rangeElement, Price.Min, Price.Max);
+range.handleEvents();
+range.to = DEFAULT_MAX_PRICE;
